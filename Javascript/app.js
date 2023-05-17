b75d35a34a7608c9bddd8f28dd939c771afecf10
famousfamous <writefamous@gmail.com>
2023-05-17 19:54:04 +0100

    now dealing with javascript

---
+++ Javascript/app.js
@@ -0,0 +1,23 @@
+let age = 32;
+let userName = "Max";
+let hobbies = ["Sporting", "Cooking", "Reading"];
+let job = { title: "Developer", place: "Vancouver", salary: 150000 };
+alert(
+  "Hi " +
+    userName +
+    ", I believe your hobbies is " +
+    hobbies[0] +
+    " but you have a total of " +
+    hobbies.length
+);
+alert(
+  "Hi, my name is " +
+    userName +
+    "I work as a " +
+    job.title +
+    " in " +
+    job.place +
+    " and my entry-level salary is " +
+    job.salary
+);
+
